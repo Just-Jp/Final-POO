c5cac84cdccf3f94ec73dd5f1674bbe8b5861c3c
Daniel Lopes <danieltayt-sohn@hotmail.com>
2025-04-23 14:15:11 -0300

    Update Dependentes.java

--- src/org/serratec/trabalhoPOO/modelos/Dependentes.java
+++ src/org/serratec/trabalhoPOO/modelos/Dependentes.java
@@ -1,14 +1,1 @@
-package org.serratec.trabalhoPOO.modelos;
 
-import java.time.LocalDate;
-
-public class Dependentes extends Pessoa {
-	public Parentesco parentesco;
-	
-	public Dependentes(String nome, String cpf, LocalDate dataNascimento, Parentesco parentesco) {
-		super(nome, cpf, dataNascimento);
-		if (dataNascimento.plusYears(18).isAfter(LocalDate.now())); {
-			this.parentesco = parentesco;
-		}
-	}
-}
